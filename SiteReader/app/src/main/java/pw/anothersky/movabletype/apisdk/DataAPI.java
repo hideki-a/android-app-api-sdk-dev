49e208adf366b54733a6604f14954b996e92cf69
Hideki Abe <hideki.abe.0630@gmail.com>
2016-11-17 17:40:42 +0900

    APIへのアクセスにOkHttp3を利用するよう変更

--- SiteReader/app/src/main/java/pw/anothersky/movabletype/apisdk/DataAPI.java
+++ SiteReader/app/src/main/java/pw/anothersky/movabletype/apisdk/DataAPI.java
@@ -1,19 +1,15 @@
 package pw.anothersky.movabletype.apisdk;
 
-import android.util.Log;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by Hideki Abe on 2016/11/17.
@@ -50,53 +46,35 @@ public class DataAPI extends JSONObject {
         return "";
     }
 
-    private HttpURLConnection makeRequest(String method, String urlString) {
-        // todo: okHttp3(http://square.github.io/okhttp/)を使うようにしたい
-        try {
-            URL url = new URL(urlString);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod(method);
-            connection.setConnectTimeout(CONNECT_TIMEOUT_MS);
-            connection.setReadTimeout(READ_TIMEOUT_MS);
-            connection.setInstanceFollowRedirects(true);
-            connection.connect();
+    private String sendRequest(String method, String url) {
+        OkHttpClient client = new OkHttpClient();
 
-            return connection;
-        } catch (IOException e) {
-            e.printStackTrace();
+        switch (method) {
+            case "GET":
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+
+                try {
+                    Response response = client.newCall(request).execute();
+                    return response.body().string();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
         }
 
         return null;
     }
 
     private JSONObject fetchList(String url) {
-        HttpURLConnection connection = this.makeRequest("GET", url);
+        String responseBody = sendRequest("GET", url);
 
-        if (connection == null) {
-            return null;
-        }
-
-        try {
-            Log.i("Test", String.valueOf(connection.getResponseCode()));
-            if (connection.getResponseCode() == 200) {
-                // 参考: http://stackoverflow.com/questions/6511880/how-to-parse-a-json-input-stream#answer-13267720
-                InputStream in = new BufferedInputStream(connection.getInputStream());
-                BufferedReader streamReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-                StringBuilder responseStringBuilder = new StringBuilder();
-
-                String inputStr;
-                while ((inputStr = streamReader.readLine()) != null) {
-                    responseStringBuilder.append(inputStr);
-                }
-
-                try {
-                    return new JSONObject(responseStringBuilder.toString());
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+        if (responseBody != null) {
+            try {
+                return new JSONObject(responseBody);
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
         return null;
