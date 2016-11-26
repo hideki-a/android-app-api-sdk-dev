b9169137a65ce02160335aa73d40dedb3677288a
Hideki Abe <hideki.abe.0630@gmail.com>
2016-11-26 14:53:50 +0900

    デプロイスクリプトを修正
    リポジトリ名を修正

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 chmod 600 ~/.ssh/id_rsa
-git clone git@github.com:hideki-a/travis-android-test.git -b master
-cp -af repository travis-android-test
-cd travis-android-test
+git clone git@github.com:hideki-a/android-app-api-sdk-dev.git -b master
+cp -af repository android-app-api-sdk-dev
+cd android-app-api-sdk-dev
 git merge origin/develop
 git add .
 git commit -m "Release by Travis CI Job $TRAVIS_JOB_NUMBER"
